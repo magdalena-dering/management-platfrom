5fc34ca1d42351275482b126b88e11fe8b1d4dad
megi <megi@macbook-pro-magda.home>
2023-07-17 10:40:10 +0200

    feat(auth): implement session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Providers from "../components/Provider";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Management Platform',
-  description: 'Management Platform',
-}
+  title: "Management Platform",
+  description: "Management Platform",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
